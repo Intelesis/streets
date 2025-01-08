d05a67f02149fd6d9f2b5c7efda0e5cf03e6f9ce
Noel Liverton <noelliverton@yahoo.com>
2025-01-08 22:12:40 +0000

    Correct column
    Low memory = false

--- filter_names.py
+++ filter_names.py
@@ -10,15 +10,18 @@ filtered_data = pd.DataFrame()
 for file in os.listdir(input_folder):
     if file.endswith(".csv"):
         file_path = os.path.join(input_folder, file)
-        data = pd.read_csv(file_path)
+        data = pd.read_csv(file_path, low_memory=False)
         
         # Filter rows for Greater London and Streets/Roads
         london_data = data[
                 (data.iloc[:, 24] == 'Greater London') &
-                (data.iloc[:, 6].str.contains('Street|Road', na=False))
+                (data.iloc[:, 7].str.contains('Street|Road', na=False))
         ]
         
         filtered_data = pd.concat([filtered_data, london_data])
+        
+pickle_file = "filtered_data.pkl"
+filtered_data.to_pickle(pickle_file)
 
 # Save the filtered data to a new CSV
 filtered_data.to_csv(output_file, index=False)
